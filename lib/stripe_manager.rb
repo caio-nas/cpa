cb94c054b46df959d13828b0a2587d342fe95fbe
Evandro A.O <evandroev@gmail.com>
2023-04-01 09:33:13 -0300

    git init

---
+++ lib/stripe_manager.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "stripe_manager/version"
+
+module StripeManager
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/stripe_manager/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module StripeManager
+  VERSION = "0.1.0"
+
+  require 'stripe'
+end
